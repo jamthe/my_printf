2c0befdd3da0ad17234eae7db92befbef4def300
elbouh_jclear <jamal.elbouhali@epitech.eu>
2015-06-30 17:13:57 +0200

    hello

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for main.c in /home/elbouh_j/PSU_2014_my_printf
+**
+** Made by jamal elbouhali
+** Login   <elbouh_j@epitech.net>
+**
+** Started on  Fri May  1 17:20:46 2015 jamal elbouhali
+** Last update Fri May  1 17:24:28 2015 jamal elbouhali
+*/
+
+#include <stdlib.h>
+
+int	main()
+{
+  my_printf("%s", NULL);
+}
